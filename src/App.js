b7b941dfa4c65edc6d16df63626f7ba9c4ece2c2
vjeuel <vjeuel@gmail.com>
2020-09-14 17:55:55 -0500

    route issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import About from './components/About';
 import Contact from './components/Contact';
@@ -8,20 +8,22 @@ import Portfolio from './components/Portfolio';
 
 function App() {
 	return (
-		<div className='App'>
-			<Navbar />
-			<Switch>
-				<Route path='/'>
-					<About />
-				</Route>
-				<Route path='/portfolio'>
-					<Portfolio />
-				</Route>
-				<Route path='/contact'>
-					<Contact />
-				</Route>
-			</Switch>
-		</div>
+		<Router>
+			<div className='App'>
+				<Navbar />
+				<Switch>
+					<Route exact path='/'>
+						<About />
+					</Route>
+					<Route path='/portfolio'>
+						<Portfolio />
+					</Route>
+					<Route path='/contact'>
+						<Contact />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
 	);
 }
 
--- src/index.js
+++ src/index.js
@@ -6,12 +6,10 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-   <React.StrictMode>
-      <Router>
-         <App />
-      </Router>
-   </React.StrictMode>,
-   document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
