9a0143e29c392f128aeda03216a8796fa082c01a
vjeuel <vjeuel@gmail.com>
2020-09-14 13:42:43 -0500

    Updates

---
+++ precache-manifest.079aa654a9bcc3a40793bb09099bf3c3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d9fbe57caa8af3c7b79254db398f4e",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "ee92ad715e0e81267d4c",
+    "url": "/portfolio-react/static/css/main.8b7a1731.chunk.css"
+  },
+  {
+    "revision": "f2d387230e3c2c3019d5",
+    "url": "/portfolio-react/static/js/2.34a33872.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-react/static/js/2.34a33872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee92ad715e0e81267d4c",
+    "url": "/portfolio-react/static/js/main.79e29db0.chunk.js"
+  },
+  {
+    "revision": "2f26d0aed242c262185b",
+    "url": "/portfolio-react/static/js/runtime-main.a4de92d3.js"
+  },
+  {
+    "revision": "7b433901f2b3066d104dcda2c0fbc454",
+    "url": "/portfolio-react/static/media/ResumeJeuel.7b433901.pdf"
+  },
+  {
+    "revision": "d23e76d2282bbd92e118cd797a2b9e9a",
+    "url": "/portfolio-react/static/media/jeuel.d23e76d2.png"
+  },
+  {
+    "revision": "bca79a6284ac37502f9bd304e16f435e",
+    "url": "/portfolio-react/static/media/logo.bca79a62.png"
+  }
+]);
